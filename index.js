05550099b95f12351edd23bdd56c01877b6a7f04
Ghyath <ghyath.darwish@gmail.com>
2021-03-05 22:30:20 +0200

    fx

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 require('dotenv').config()
 const {Telegraf} = require('telegraf');
-const express = require('express');
+// const express = require('express');
 let bot = new Telegraf(process.env.BOT_TOKEN)
 bot.launch()
-const app = express()
+// const app = express()
 
-app.get('/getfile',(req,res)=>{
-	if(!req.query || req.query.pass !== 'lilili123')return res.send(false);
-	res.sendFile(__dirname+'/movies.json')
-})
+// app.get('/getfile',(req,res)=>{
+// 	if(!req.query || req.query.pass !== 'lilili123')return res.send(false);
+// 	res.sendFile(__dirname+'/movies.json')
+// })
 
 const Scraper = require('./scraper');
 let scraper;
@@ -29,9 +29,9 @@ bot.command('getfile',(ctx)=>{
 	if(ctx.from.id != 566571423)return;
 	ctx.replyWithDocument({source:'./movies.json'})
 })
-app.listen((process.env.PORT || 4000),()=>{
-	console.log('Listen started');
-})
+// app.listen((process.env.PORT || 4000),()=>{
+// 	console.log('Listen started');
+// })
 
 process.once('SIGINT', () => bot.stop('SIGINT'))
 process.once('SIGTERM', () => bot.stop('SIGTERM'))
