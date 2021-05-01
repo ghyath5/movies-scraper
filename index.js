6e724c74dffb7beb96c632823a6c12281887e363
Ghyath <ghyath.darwish@gmail.com>
2021-05-01 23:04:47 +0300

    fx

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const {Telegraf} = require('telegraf');
+const { Telegraf } = require('telegraf');
 // const express = require('express');
 let bot = new Telegraf(process.env.BOT_TOKEN)
 bot.launch()
@@ -13,25 +13,22 @@ bot.launch()
 const Scraper = require('./scraper');
 let scraper;
 
-bot.command('starts',(ctx)=>{
-	if(ctx.from.id != 566571423)return;
+bot.command('starts', (ctx) => {
+	if (ctx.from.id != 566571423) return;
 	scraper = new Scraper(bot)
 	scraper.start()
 	ctx.reply('Started')
 })
 
-bot.command('stop',(ctx)=>{
-	if(ctx.from.id != 566571423)return;
+bot.command('stop', (ctx) => {
+	if (ctx.from.id != 566571423) return;
 	scraper.stop()
 	ctx.reply('stopped')
 })
-bot.command('getfile',(ctx)=>{
-	if(ctx.from.id != 566571423)return;
-	ctx.replyWithDocument({source:'./movies.json'})
+bot.command('getfile', (ctx) => {
+	if (ctx.from.id != 566571423) return;
+	ctx.replyWithDocument({ source: './movies.json' })
 })
-// app.listen((process.env.PORT || 4000),()=>{
-// 	console.log('Listen started');
-// })
 
 process.once('SIGINT', () => bot.stop('SIGINT'))
 process.once('SIGTERM', () => bot.stop('SIGTERM'))
